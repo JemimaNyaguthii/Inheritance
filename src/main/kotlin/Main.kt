ab2d1d967f4f2b76daa65d09ca0ff7aa6e829667
JemimaNyaguthii <jemimanyaguthiiwambugu@gmail.com>
2023-03-08 08:30:58 +0300

    fifth commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,14 +7,7 @@ fun main(args: Array<String>) {
     println(noah.maxTripFare(100.0))
     println(noah.calculateParkingFees(3))
 }
-
-
-//Create a class Car with the following attributes: make, model, color, capacity.
-//It has these functions:
-//- carry(people: Int) : Prints out “Carrying $people passengers” if the
-//number of people is within its capacity else it prints out “Over capacity
-//by $x people” where x is the number of people exceeding its capacity
-open class Car(var make:String,var model:String,var color:String,var capacity:Int) {
+open class Vehicle(var make:String,var model:String,var color:String,var capacity:Int) {
     fun carry(people: Int) {
         var x = people - capacity
         if (people <= capacity) {
@@ -23,21 +16,24 @@ open class Car(var make:String,var model:String,var color:String,var capacity:In
             println("Over capacity by $x people")
         }
     }
-
-//    identity() : Prints out the color, make and model in the following
-//    format e.g: “I am a white subaru legacy”
-
     fun identity() {
         println("I am a $color $make $model ")
     }
-
-    //calculateParkingFees(hours: Int) : Calculates and returns the
-//parking fees by multiplying the hours by 20
     open fun calculateParkingFees(hours: Int): Int {
         var product = hours * 20
         return product
 
     }
+}
+
+
+//Create a class Car with the following attributes: make, model, color, capacity.
+//It has these functions:
+//- carry(people: Int) : Prints out “Carrying $people passengers” if the
+//number of people is within its capacity else it prints out “Over capacity
+//by $x people” where x is the number of people exceeding its capacity
+class Car( make:String,model:String,color:String, capacity:Int):Vehicle(make,model,color,capacity,){
+
 }
 
 //    Create a class Bus with the same attributes and functions as the Car class.
@@ -47,7 +43,7 @@ open class Car(var make:String,var model:String,var color:String,var capacity:In
 //    The bus’ calculateParkingFees method returns the product of hours and
 //    the capacity of the bus (3 points)
 
-    class Bus(make: String,model: String,color: String,capacity: Int,):Car(make,model,color,capacity,) {
+    class Bus(make: String,model: String,color: String,capacity: Int,):Vehicle(make,model,color,capacity,) {
         fun maxTripFare(fare:Double):Double{
             var total=fare*capacity
             return total
